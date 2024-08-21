0cb7e49713b87bcca092bb6da66de884688f9e8f
Jeremy Mayeres <jeremy@jerr.dev>
2024-08-21 16:33:00 +0300

    ✨ Add audit log validation
    Enforces the following concepts if the outcome result is SUCCEEDED:
    1. The resource_id cannot be null
    2. CREATE actions need `after` to be not null
    3. UPDATE actions need `before` and `after` to be not null
    4. DELETE actions need `before` to be not null
    
    Bonus changes:
    - Upgrade pre-commit hooks
    - Ensure there's a timeout set for the release workflow

--- audit_log/exceptions.py
+++ audit_log/exceptions.py
@@ -2,5 +2,9 @@ class AuditError(Exception):
     """General error in audit library"""
 
 
+class AuditValidationError(Exception):
+    """Audit logging called without proper data"""
+
+
 class AuditPrincipalError(AuditError):
     """Error with the principal"""
